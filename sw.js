0f10c18dcfa1779511a61c7f699158335f5ca098
meklitmitik03-creator <meklitmitik03@gmail.com>
2026-03-29 18:03:39 +0000

    add new file culture.html and update index.html and sw.js to include culture.html in the navigation and caching respectively.

--- sw.js
+++ sw.js
@@ -1,4 +1,28 @@
 // sw.js - Service Worker
+const CACHE_NAME = 'ethio-travel-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  'https://cdn-icons-png.flaticon.com/512/330/330551.png'
+];
+
+// 1. Install Event: Saves your files so the app works offline
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching app assets');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// 2. Fetch Event: Helps the app load instantly from the cache
 self.addEventListener('fetch', (event) => {
-    // This allows the app to load faster
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
